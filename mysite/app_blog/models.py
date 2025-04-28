83439d09b492d3e86de653900f93f0b2dd1d8ecf
Olena098 <144223584+Olena098@users.noreply.github.com>
2025-04-28 17:04:07 +0300

    Розробка серверної частини  персонального блогу.

--- mysite/app_blog/models.py
+++ mysite/app_blog/models.py
@@ -11,6 +11,7 @@ class Category(models.Model):
         help_text=u'Максимум 250 символів'
     )
     slug = models.SlugField(u'Слаг')
+    objects = models.Manager()
 
     class Meta:
         verbose_name = u'Категорія для публікації'
@@ -18,6 +19,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category
+
+    def get_absolute_url(self):
+        try:
+            url = reverse('articles-category-list', kwargs={'slug': self.slug})
+        except:
+            url = "/"
+        return url
 
 
 class Article(models.Model):
--- mysite/app_blog/urls.py
+++ mysite/app_blog/urls.py
@@ -1,7 +1,15 @@
-from django.contrib import admin
 from django.urls import path
 
+from .views import (
+    HomePageView,
+    ArticleDetail,
+    ArticleList,
+    ArticleCategoryList
+)
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Ваші інші URL-шляхи
+    path('', HomePageView.as_view(), name='home'),
+    path('articles/', ArticleList.as_view(), name='articles-list'),
+    path('articles/category/<slug>/', ArticleCategoryList.as_view(), name='articles-category-list'),
+    path('articles/<year>/<month>/<day>/<slug>/', ArticleDetail.as_view(), name='news-detail'),
 ]
